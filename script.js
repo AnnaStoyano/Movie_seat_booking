9246f592e975081745f65c33a75620595a5186df
Anna Stoyanova <annstoyanova99@gmail.com>
2021-02-20 21:12:39 +0200

    Fix

--- script.js
+++ script.js
@@ -4,36 +4,36 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 function bookTicket() {
     const hallForm = document.querySelector("#seats");
-    const seatsListHall= document.querySelector('.cinema-hall-wrapper .selected-seats--list');
+    const seatsListHall = document.querySelector('.cinema-hall-wrapper .selected-seats--list');
     const popupWrapper = document.querySelector('.popup__wrapper');
     const seats = [];
-    hallForm.addEventListener('change', ({ target }) => {
+    hallForm.addEventListener('change', ({target}) => {
         if (target.name == 'seat') {
-            selectSeats(seats, target,seatsListHall);
+            selectSeats(seats, target, seatsListHall);
         }
     });
     hallForm.addEventListener('click', e => {
         if (e.target.type == 'submit') {
             e.preventDefault();
-            buyTicket(seats,popupWrapper);
+            buyTicket(seats, popupWrapper);
         }
     });
 }
 
-function selectSeats(seats, target,showedSeatsList) {
+function selectSeats(seats, target, showedSeatsList) {
     if (target.checked) {
         seats.push(target.value);
     } else if (!target.checked) {
         const seatIndex = seats.indexOf(target);
         seats.splice(seatIndex, 1);
     }
-    fillSeatsInfo(seats,showedSeatsList);
+    fillSeatsInfo(seats, showedSeatsList);
 }
 
-function buyTicket(seats,popup__wrapper) {
+function buyTicket(seats, popup__wrapper) {
     showWrapper(popup__wrapper);
     closeBotton(popup__wrapper);
-    fillSeatsInfo(seats,popup__wrapper.querySelector('.selected-seats--list'));
+    fillSeatsInfo(seats, popup__wrapper.querySelector('.selected-seats--list'));
 }
 
 function fillSeatsInfo(seats, seatsList) {
@@ -43,7 +43,7 @@ function fillSeatsInfo(seats, seatsList) {
         const checkedSeatsString = seats.map(seat => `<li>${seat}</li>`).join('\n');
         seatsList.innerHTML = checkedSeatsString;
         showWrapper(seatsWrapper);
-    } else{
+    } else {
         seatsWrapper.classList.add('hidden');
     }
 }
@@ -54,7 +54,7 @@ function showWrapper(displayWrapper) {
     }
 }
 
-function closeBotton(wrapper){
+function closeBotton(wrapper) {
     const closeButton = wrapper.querySelector('.close');
-    closeButton.addEventListener('click',()=> wrapper.classList.add('hidden'));
+    closeButton.addEventListener('click', () => wrapper.classList.add('hidden'));
 }
